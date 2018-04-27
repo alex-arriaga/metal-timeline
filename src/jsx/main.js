ff9e765831b1ea78f2441fabddef014b0105b95f
Alex Arriaga <alex.arriaga@base22.com>
2018-04-27 16:48:49 -0400

    Create basic timeline component

---
+++ src/jsx/main.js
@@ -0,0 +1,14 @@
+import JSXComponent from 'metal-jsx';
+
+class Timeline extends JSXComponent {
+  render() {
+    return (
+        <div class="m-timeline-component">
+          Timeline Component
+        </div>
+    );
+  }
+}
+
+export {Timeline};
+export default Timeline;
