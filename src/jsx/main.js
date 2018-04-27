b1f67df29a0f3a656c721455d6f3423cc2b50812
Alex Arriaga <alex.arriaga@base22.com>
2018-04-27 17:56:12 -0400

    Add example of rendering STATE

--- src/jsx/main.js
+++ src/jsx/main.js
@@ -5,34 +5,52 @@ import './styles.scss';
 class Timeline extends JSXComponent {
   render() {
     return (
-        <div class="metal-item metal-item-timeline">
-          <div class="metal-item metal-item-timeline__info-container">
-            <div class="metal-item metal-item-timeline__image-container">
-              <img src="http://lorempixel.com/400/400/people/1/"
-                   class="metal-item metal-item-timeline__image"/>
-              <span class="metal-item metal-item-timeline__date">09.12.2016</span>
-            </div>
-          </div>
-          <div class="metal-item metal-item-timeline__details-container">
-            <div class="metal-item metal-item-timeline__details">
-              <h3 class="metal-item metal-item-timeline__title">Phasellus pharetra</h3>
-              <div class="metal-item metal-item-timeline__description">Proin condimentum tempus
-                ultrices. Suspendisse vestibulum
-                suscipit erat, ac efficitur lorem. Nullam non ex vel turpis
-                imperdiet maximus sit amet nec odio. Donec mauris nisl,
-                vestibulum id efficitur at, convallis id dui. Sed enim nisl,
-                ultrices vitae sodales eu, vestibulum a mi. Morbi consectetur
-                pulvinar sagittis. Phasellus pharetra diam id leo gravida
-                pharetra. In rutrum est gravida, maximus mi ac, mattis metus. Ut
-                at tempus sem. Vivamus condimentum erat eget aliquet
-                dignissim.
-              </div>
-            </div>
+        <div className="metal-item-timeline">
+          <div class="metal-item-timeline__items">
+            {this.state.items.map(
+                (item) => {
+                  return <div class="metal-item metal-item-timeline">
+                    <div class="metal-item metal-item-timeline__info-container">
+                      <div
+                          class="metal-item metal-item-timeline__image-container">
+                        <img src="http://lorempixel.com/400/400/people/1/"
+                             class="metal-item metal-item-timeline__image"
+                             alt=""/>
+                        <span
+                            class="metal-item metal-item-timeline__date">{item.date}</span>
+                      </div>
+                    </div>
+                    <div
+                        class="metal-item metal-item-timeline__details-container">
+                      <div class="metal-item metal-item-timeline__details">
+                        <h3 class="metal-item metal-item-timeline__title">
+                          {item.title}
+                        </h3>
+                        <div
+                            class="metal-item metal-item-timeline__description">
+                          {item.description}
+                        </div>
+                      </div>
+                    </div>
+                  </div>;
+                })
+            }
           </div>
         </div>
     );
   }
 }
+
+Timeline.STATE = {
+  items: {
+    value: [
+      {
+        title: 'Sample #1'
+      }, {
+        title: 'Sample #2'
+      }]
+  }
+};
 
 defineWebComponent('metal-timeline', Timeline);
 
